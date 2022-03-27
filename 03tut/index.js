0f477a843a814d4210c5ec68e5b701e78d6e982c
nhantringuyen <nhantringuyen893109@gmail.com>
2022-03-27 23:01:28 +0700

    second commit

--- 03tut/index.js
+++ 03tut/index.js
@@ -9,7 +9,21 @@
 //   }
 //npm run dev
 //npm i uuid
-const {format}  = require('date-fns');
-const {v4: uuid} = require('uuid');
-console.log(format(new Date(), 'yyyyMMdd\tHH:mm:ss'));
-console.log(uuid())
+// const {format}  = require('date-fns');
+// const {v4: uuid} = require('uuid');
+// console.log(format(new Date(), 'yyyyMMdd\tHH:mm:ss'));
+// console.log(uuid())
+const logEvents = require('./logEvents');
+const EventEmitter = require('events');
+class MyEmitter extends EventEmitter{};
+
+//initialize object
+const myEmitter = new MyEmitter();
+
+//add listener for the log event
+myEmitter.on('log',(msg) => logEvents(msg));
+
+setTimeout(() => {
+    //Emit event
+    myEmitter.emit('log', 'Log event emitted!');
+},2000);
