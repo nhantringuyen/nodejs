150c60527cbfffb8e39ce388c90e3698401d71d8
nhantringuyen <nhantringuyen893109@gmail.com>
2022-03-27 11:12:42 +0700

    first commit

--- 03tut/index.js
+++ 03tut/index.js
@@ -1,4 +1,7 @@
 //npm i nodemon -g
 //npm init
 //npm i date-fns
-console.log("Testing!");
+// console.log("Testing!");
+//npm i nodemon --save-dev / npm i nodemon -D
+const {format}  = require('date-fns');
+console.log(format(new Date(), 'yyyyMMdd\tHH:mm:ss'));
